8443d42e59a9a2aeb7a6149689338ffbe62353f8
inmove <lisper.inmove@gmail.com>
2024-03-17 00:03:21 +0800

    optimize home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,10 @@ import Link from "next/link";
 import { ReactElement, useEffect, useState } from "react";
 import SelfIntro from "@/components/SelfIntro";
 import LoadingComponent from "@/components/LoadingComponent";
+import { cn } from "@/utils/cn";
 
 export default function Resume() {
   let [opened, setOpened] = useState(false);
-  function handleClick() {
-    const selfIntroDom = document.getElementById("self-intro");
-    const resumeDom = document.getElementById("resume");
-    if (opened) {
-      resumeDom && (resumeDom.style.transform = "translateX(0px)");
-      if (selfIntroDom) {
-        selfIntroDom.style.transform = "translateX(0px)";
-        selfIntroDom.style.opacity = "0";
-      }
-      setOpened(false);
-    } else {
-      resumeDom && (resumeDom.style.transform = "translateX(-200px)");
-      if (selfIntroDom) {
-        selfIntroDom.style.transform = "translateX(200px)";
-        selfIntroDom.style.opacity = "1";
-      }
-      setOpened(true);
-    }
-  }
 
   let [c, setc] = useState<ReactElement | null>(null);
   useEffect(() => {
@@ -42,70 +24,72 @@ export default function Resume() {
         Math.random() * (1000 - 500) + 500,
       );
     });
-
-    const timer = setTimeout(() => {
-      handleClick();
-    }, 2000);
-
-    // Cleanup the timer when the component unmounts
-    return () => clearTimeout(timer);
   }, []);
 
   return c == null ? (
     <LoadingComponent></LoadingComponent>
   ) : (
-    <div id="resume-div" className="w-[100vw] h-[100vh]">
-      <div className="overlay">
-        <div
-          id="self-intro"
-          className="flex flex-col self-intro absolute w-96 h-1/2 backdrop-blur-xl border rounded-3xl border-transparent justify-center items-center opacity-0 p-10"
-        >
-          {c}
+    <div
+      id="resume-div"
+      className="w-[100vw] h-[100vh] flex justify-center items-center"
+    >
+      {/* self intro */}
+      <div
+        id="self-intro"
+        className={cn(
+          "flex flex-col self-intro absolute w-96 h-1/2 backdrop-blur-xl border rounded-3xl border-transparent justify-center items-center p-10 transition-all opacity-0",
+          { "translate-x-1/2 opacity-100": opened },
+        )}
+      >
+        {c}
+      </div>
+
+      {/* avatar */}
+      <div
+        className={cn(
+          "grid grid-cols-1 resume backdrop-blur-xl border rounded-3xl border-transparent w-96 h-1/2 justify-center absolute",
+          { "-translate-x-1/2": opened },
+        )}
+        onClick={() => setOpened(!opened)}
+        id="resume"
+      >
+        <div className="avatar-image m-auto mt-12">
+          <Image
+            src="https://inmove-blog.oss-cn-hangzhou.aliyuncs.com/images/avatar.jpg"
+            alt="avatar"
+            width={150}
+            height={150}
+            className="rounded-full"
+          ></Image>
         </div>
         <div
-          className="grid grid-cols-1 resume backdrop-blur-xl border rounded-3xl border-transparent w-96 h-1/2 justify-center absolute"
-          onClick={() => handleClick()}
-          id="resume"
+          className={`avatar-title ${lfont.className} text-orange-800 text-3xl text-center mt-20`}
         >
-          <div className="avatar-image m-auto mt-12">
-            <Image
-              src="https://inmove-blog.oss-cn-hangzhou.aliyuncs.com/images/avatar.jpg"
-              alt="avatar"
-              width={150}
-              height={150}
-              className="rounded-full"
-            ></Image>
+          <h3>Hello I&apos;m inmove</h3>
+          <h3>Full Stack Programmer</h3>
+        </div>
+        <div className="intro-icons grid-cols-4 text-3xl text-center items-center">
+          <div className="intro-icon">
+            <Link
+              href="https://github.com/lisper-inmove"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <BsGithub></BsGithub>
+            </Link>
           </div>
-          <div
-            className={`avatar-title ${lfont.className} text-orange-800 text-3xl text-center mt-20`}
-          >
-            <h3>Hello I&apos;m inmove</h3>
-            <h3>Full Stack Programmer</h3>
-          </div>
-          <div className="intro-icons grid-cols-4 text-3xl text-center items-center">
-            <div className="intro-icon">
-              <Link href="https://github.com/lisper-inmove">
-                <BsGithub></BsGithub>
-              </Link>
-            </div>
-            <div className="intro-icon">
-              <Link href="https://inmove.top/site?c=Projects">
-                <FaBlog></FaBlog>
-              </Link>
-            </div>
+          <div className="intro-icon">
+            <Link
+              href="https://inmove.top/site?c=Projects"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <FaBlog></FaBlog>
+            </Link>
           </div>
         </div>
-        {/*
-        <div className="bubble-list">
-          <div className="bubble">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        */}
       </div>
     </div>
   );
