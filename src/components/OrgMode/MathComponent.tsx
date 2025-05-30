bc3d4e864c876281526fcfbaeec94cf081b7509f
inmove <lisper.inmove@gmail.com>
2025-05-30 15:13:17 +0800

    done

--- src/components/OrgMode/MathComponent.tsx
+++ src/components/OrgMode/MathComponent.tsx
@@ -8,8 +8,19 @@ const config = {
 
 export default function MathComponent({ content }: { content: string }) {
     return (
-        <MathJaxContext version={3} config={config}>
-            <MathJax>{content}</MathJax>
-        </MathJaxContext>
+        <div className="flex m-5">
+            <MathJaxContext version={3} config={config}>
+                <MathJax
+                    className="border border-gray-300 pl-2 pr-2 rounded-2xl bg-amber-100 dark:bg-gray-600"
+                    style={{
+                        boxShadow:
+                            "white -3px -3px 4px 2px, #7e7e7e 3px 3px 4px 2px",
+                        borderRadius: "10px",
+                    }}
+                >
+                    {content}
+                </MathJax>
+            </MathJaxContext>
+        </div>
     );
 }
