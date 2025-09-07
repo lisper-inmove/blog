869c4b86978a0d32d9c0709e1c83f1e005fffc40
inmove <lisper.inmove@gmail.com>
2025-09-07 16:06:37 +0800

    done

--- src/app/demonstration/transition/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function TransitionDemoPage() {
-  return (
-    <>
-      <div className="w-[100vw] h-[100vh] transition-test">
-        Hover Change Color
-      </div>
-    </>
-  );
-}
